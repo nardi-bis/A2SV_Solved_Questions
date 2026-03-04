e5079b58506f1c43e0c05f8c8d09c2e930d34c8c
Nardos Bisrat <nardosbisrat944@gmail.com>
2026-03-04 17:13:53 +0300

    Time: 51 ms (75.47%) | Memory: 39.6 MB (44.00%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,18 +1,14 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
         # if the remainder of two prefix sum when divided by k is the same then subarray between them is divisible by k
-
-        remainder_map = {0: -1}   # remainder : index
+        hashmap = {0:-1} # remainder, index
         runsum = 0
-
         for i, num in enumerate(nums):
             runsum += num
             remainder = runsum % k
-
-            if remainder in remainder_map:
-                if i - remainder_map[remainder] > 1:
+            if remainder in hashmap:
+                if i - hashmap[remainder] > 1:
                     return True
             else:
-                remainder_map[remainder] = i
-
+                hashmap[remainder] = i
         return False
