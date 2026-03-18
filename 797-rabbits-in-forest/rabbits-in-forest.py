a8d00ab8a207efb1835a8d30701ff67982a733cd
Nardos Bisrat <nardosbisrat944@gmail.com>
2026-03-18 12:32:49 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (91.95%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.py
+++ 797-rabbits-in-forest/rabbits-in-forest.py
@@ -1,17 +1,17 @@
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
-        hashmap = {}
+        hasm = {}
         total = 0
-
         for a in answers:
-            if a not in hashmap:
-                hashmap[a] = a   # remaining spots in this group
-                total += a + 1   # create a new group
+            if a in hasm:
+                hasm[a] -= 1
+                if hasm[a] < 0:
+                    hasm[a] = a
+                    total += a+1
             else:
-                hashmap[a] -= 1  # fill the group
-
-                if hashmap[a] < 0:
-                    hashmap[a] = a   # start a new group
-                    total += a + 1
-
+                hasm[a] = a
+                total += a+1
         return total
+
+
+        
