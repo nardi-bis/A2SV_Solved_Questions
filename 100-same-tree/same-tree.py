e3821b33bd7a3e4d841df112be426fcd47280a7d
Nardos Bisrat <nardosbisrat944@gmail.com>
2026-03-23 19:13:24 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (98.02%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,13 +6,9 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        from typing import Optional
-
-
-class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:  # Both nodes are None
+        if not q and not p:
             return True
-        if p and q and p.val == q.val:  # Both nodes exist and have the same value
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        return False  # One of the nodes is None or their values differ
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(q.right, p.right)
+        return False
+
