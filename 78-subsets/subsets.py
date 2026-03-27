68967c2bf1a770a6de5de383456126bf3e051fcf
Nardos Bisrat <nardosbisrat944@gmail.com>
2026-03-27 14:47:06 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (51.49%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,11 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        def back(start, path):
-            ans.append(path[:])
-            for i in range(start,len(nums)):
-                path.append(nums[i])
-                back(i+1,path)
-                path.pop()
-        back(0, [])
+        subset = []
+        def back(i):
+            if i >= len(nums):
+                ans.append(subset[:])
+                return
+            subset.append(nums[i])
+            back(i+1)
+
+            subset.pop()
+            back(i+1)
+
+          
+        back(0)
         return ans
