9be06a7a58cdf318eea2c34d25f7a1dea8fc7a2b
Nardos Bisrat <nardosbisrat944@gmail.com>
2026-04-07 17:23:35 +0300

    Time: 816 ms (16.05%) | Memory: 27 MB (65.55%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -3,14 +3,14 @@ class Solution:
         def merge(arr1, arr2):
             merged = []
             i, j = 0, 0
-            while i < len(arr1) and j < len(arr2):
-                if arr1[i] < arr2[j]:
+            while i < len(arr1) or j < len(arr2):
+                if j == len(arr2) or (i < len(arr1) and arr1[i] < arr2[j]):
                     merged.append(arr1[i])
                     i += 1
                 else:
                     merged.append(arr2[j])
                     j += 1
-            return merged + arr1[i:] + arr2[j:]
+            return merged 
 
         def msort(arr):
             if len(arr) == 1:
