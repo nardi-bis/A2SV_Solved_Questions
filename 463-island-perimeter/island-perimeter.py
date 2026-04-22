4e9135962fcea5ef1b6ac0af11f125be084a4098
Nardos Bisrat <nardosbisrat944@gmail.com>
2026-04-22 16:52:24 +0300

    Time: 208 ms (5.20%) | Memory: 23.3 MB (17.86%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -2,34 +2,39 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         row = len(grid)
         col = len(grid[0])
-        ans = 0
-      
+        def inbound(r, c):
+            return 0 <= r < row and 0 <= c <col
 
-        def inbound(row, col):
-            return (0 <= row < len(grid) and 0 <= col < len(grid[0]))
+        visited = [[False] * col for i in range(row)]
+
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+
+        def dfs(r, c):
+            if not inbound(r, c):
+                return 1
+            if grid[r][c] == 0:
+                return 1
+            if visited[r][c]:
+                return 0
+            
+            visited[r][c] = True # next if we come back again we will return 0 
+            perimeter = 0
+
+            for r_change, c_change in directions:
+                new_row = r + r_change
+                new_col = c + c_change
+
+                perimeter += dfs(new_row, new_col)            
+            print(perimeter)   
+            return perimeter
+            
+        # to find the first land
         for r in range(row):
             for c in range(col):
-                if grid[r][c] == 0:
-                    continue
-                if grid[r][c] == 1 :
-                    ans += 4
-                if inbound(r-1,c) and grid[r-1][c] == 1:
-                    ans -= 1
-                if inbound(r+1,c) and grid[r+1][c] == 1:
-                    ans -= 1
-                if inbound(r,c-1) and grid[r][c-1] == 1:
-                    ans -= 1
-                if inbound(r,c+1) and grid[r][c+1] == 1:
-                    ans -= 1
-                print(r, c, ans)
-                # if grid[r-1][c] and grid[r-1][c] == 1:
-                #     ans -= 1
-                # if grid[r+1][c] and grid[r+1][c] == 1:
-                #     ans -= 1
-                # if grid[r][c-1] and grid[r][c-1]== 1:
-                #     ans -= 1
-                # if grid[r][c+1] and grid[r][c+1] == 1:
-                #     ans -= 1
-                
-        return ans
+                if grid[r][c] == 1:
+                    return dfs(r, c) 
+        return 0
+
+
+
         
