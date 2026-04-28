4ccd9d2c8b3e4e589a8bab6a4a1780b9f941ef7b
Nardos Bisrat <nardosbisrat944@gmail.com>
2026-04-28 12:25:22 +0300

    Time: 76 ms (99.24%) | Memory: 33.6 MB (72.03%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,12 +1,29 @@
 class NumMatrix:
+
     def __init__(self, matrix: List[List[int]]):
-        n = len(matrix)
-        m = len(matrix[0])
-        self.pre = [[0] * (m+2) for _ in range(n+2)]
+        row = len(matrix)
+        col = len(matrix[0])
+        
+        self.sumMat = [[0] * (col + 1) for i in range(row + 1)]
 
-        for i in range(1, n+1):
-            for j in range(1, m+1):
-                self.pre[i][j] = self.pre[i-1][j] + self.pre[i][j-1] - self.pre[i-1][j-1] + matrix[i-1][j-1]
-
+        for i in range(row):
+            prefix = 0
+            for j in range(col):
+                prefix += matrix[i][j]
+                above = self.sumMat[i][j + 1]# was supposed to be self.sumMat[i - 1][j] but there is and additional row and col added with the value of 0
+                self.sumMat[i + 1][j + 1] = prefix + above
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return self.pre[row2+1][col2+1] - self.pre[row2+1][col1] - self.pre[row1][col2+1] + self.pre[row1][col1]
+        r1, c1, r2, c2 = row1 + 1, col1 + 1, row2 + 1, col2 + 1
+        bottomright = self.sumMat[r2][c2]
+        # the above row that is going to be subtracted
+        above = self.sumMat[r1 - 1][c2]
+        #the left col that is going to be subtracted
+        left = self.sumMat[r2][c1 - 1] 
+        topleft = self.sumMat[r1 - 1][c1 - 1] 
+        return bottomright - above - left + topleft
+        
+
+
+# Your NumMatrix object will be instantiated and called as such:
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
