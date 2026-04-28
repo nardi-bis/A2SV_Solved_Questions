352843cd879dfac75c043b0aca1cc3ca9956d60f
Nardos Bisrat <nardosbisrat944@gmail.com>
2026-04-28 15:26:56 +0300

    Time: 43 ms (22.32%) | Memory: 22.5 MB (17.80%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,36 +1,37 @@
 class Solution:
-    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        directions = [(-1,0), (1,0), (0, 1), (0, -1)]
-        n = len(heights)
-        m = len(heights[0])
+    def pacificAtlantic(self, height: List[List[int]]) -> List[List[int]]:
+        row = len(height)
+        col = len(height[0])
         pac = set()
         atl = set()
 
-        def inbound(row, col):
-            return 0 <= row < n and 0 <= col < m
+        def inbound(r, c):
+            return 0 <= r < row and 0 <= c < col
+        directions = [[1, 0],[0, 1],[-1, 0],[0, -1]]
 
-        def dfs(row, col, visited):
-            visited.add((row, col))
+        def dfs(r, c, visited):
+            visited.add((r, c)) # why not check if it is inbound 
 
-            for a,b in directions:
-                nrow = row + a
-                ncol = col + b
-
-                if inbound(nrow, ncol):
-                    if (nrow, ncol) not in visited and heights[row][col] <= heights[nrow][ncol]:
-                        dfs(nrow, ncol, visited)
-
-        for i in range(m): 
+            for a, b in directions:
+                new_r = r + a
+                new_c = c + b
+                if inbound(new_r, new_c) and (new_r, new_c) not in visited and height[r][c] <= height[new_r] [new_c]:
+                    dfs(new_r, new_c, visited)
+        
+        for i in range(row): # the left and the right
+            dfs(i, 0, pac) 
+            dfs(i, col - 1, atl) 
+        for i in range(col): # to the top and the down
             dfs(0, i, pac)
-            dfs(n-1, i, atl)
-        for i in range(n):
-            dfs(i, 0, pac)
-            dfs(i, m-1, atl)
+            dfs(row - 1, i, atl)
 
         res = []
-        for i in range(n):
-            for j in range(m):
+        for i in range(row):
+            for j in range(col):
                 if (i,j) in pac and (i,j) in atl:
                     res.append([i,j])
-
         return res
+
+
+
+        
