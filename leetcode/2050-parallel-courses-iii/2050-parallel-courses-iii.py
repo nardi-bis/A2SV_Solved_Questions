0755d298772f20f9b1b52acab8c5d93e2f89d96d
Nardos Bisrat <nardosbisrat944@gmail.com>
2026-05-05 23:29:56 +0300

    Add solution for Parallel Courses III

--- leetcode/2050-parallel-courses-iii/2050-parallel-courses-iii.py
+++ leetcode/2050-parallel-courses-iii/2050-parallel-courses-iii.py
@@ -1,27 +1,26 @@
 class Solution:
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
-        adj = [[] for _ in range(n + 1)]
+        adj = [[] for i in range(n + 1)]
         indeg = [0] * (n + 1)
-        q = deque()
-        for fro, to in relations:
-            adj[fro].append(to)
+        for prev, to in relations:
+            adj[prev].append(to)
             indeg[to] += 1
+        q = deque()
 
         for i in range(1, n + 1):
             if indeg[i] == 0:
                 q.append(i)
         month = [0] * (n + 1)
         while q:
-            for _ in range(len(q)): # level
+            for i in range(len(q)):
                 node = q.popleft()
                 month[node] += time[node - 1]
-                for i in adj[node]: # for the lijs
-                    indeg[i] -= 1
-                    if indeg[i] == 0:
-                        q.append(i)
-                    month[i] = max(month[node], month[i])
+                for child in adj[node]:
+                    indeg[child] -= 1
+                    month[child] = max(month[node], month[child])
+                    if indeg[child] == 0:
+                        q.append(child)
         return max(month)
-                
 
 
         
