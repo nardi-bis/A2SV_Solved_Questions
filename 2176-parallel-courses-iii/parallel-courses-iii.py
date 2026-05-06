4ccd28ac22a08f3027a372632c17783990154778
Nardos Bisrat <nardosbisrat944@gmail.com>
2026-05-06 23:36:57 +0300

    Time: 79 ms (99.09%) | Memory: 58 MB (20.33%) - LeetSync

--- 2176-parallel-courses-iii/parallel-courses-iii.py
+++ 2176-parallel-courses-iii/parallel-courses-iii.py
@@ -1,26 +1,34 @@
 class Solution:
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
-        adj = [[] for i in range(n + 1)]
-        indeg = [0] * (n + 1)
-        for prev, to in relations:
-            adj[prev].append(to)
-            indeg[to] += 1
-        q = deque()
 
+        graph = [[] for i in range(n + 1)]
+
+        for pre, course in relations:
+            graph[course].append(pre)
+        
+        complete_times = [0 for i in range(n + 1)]
+
+        def dfs(course):
+            nonlocal complete_times
+
+            if complete_times[course]:
+                return complete_times[course]
+            
+            max_prereq = 0
+            
+            for pre in graph[course]:
+                max_prereq = max(max_prereq, dfs(pre))
+            
+            complete_times[course] = max_prereq + time[course - 1]
+            
+            return complete_times[course]
+        
         for i in range(1, n + 1):
-            if indeg[i] == 0:
-                q.append(i)
-        month = [0] * (n + 1)
-        while q:
-            for i in range(len(q)):
-                node = q.popleft()
-                month[node] += time[node - 1]
-                for child in adj[node]:
-                    indeg[child] -= 1
-                    month[child] = max(month[node], month[child])
-                    if indeg[child] == 0:
-                        q.append(child)
-        return max(month)
-
+            if not complete_times[i]:
+                dfs(i)
+        
+        return max(complete_times)
+        
+        
 
         
