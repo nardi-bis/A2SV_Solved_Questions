8d186b60e83bc02053c219bc44dd674e4d54c95a
Nardos Bisrat <nardosbisrat944@gmail.com>
2026-05-21 12:28:31 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (86.83%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -4,7 +4,7 @@ class Solution:
         def dp(x):
             if x == 0:
                 return 0
-            if x == 1 or x == 2:
+            if x <= 2:
                 return 1
             if x not in memo:
                 d = x - 3
