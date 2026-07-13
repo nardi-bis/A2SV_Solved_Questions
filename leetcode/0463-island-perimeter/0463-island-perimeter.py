2fd59a3e522a6e440412332a55ec66d907110f08
Nardos Bisrat <nardosbisrat944@gmail.com>
2026-07-13 23:41:32 +0300

    Add solution for Island Perimeter

--- leetcode/0463-island-perimeter/0463-island-perimeter.py
+++ leetcode/0463-island-perimeter/0463-island-perimeter.py
@@ -1,34 +1,35 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
+      
         row = len(grid)
         col = len(grid[0])
+     
+
+
         def inbound(r, c):
             return 0 <= r < row and 0 <= c <col
 
         visited = [[False] * col for i in range(row)]
 
-        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        direction = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
-        def dfs(r, c):
+        def dfs(r, c): # we re doing this when it is land
             if not inbound(r, c):
                 return 1
             if grid[r][c] == 0:
                 return 1
             if visited[r][c]:
                 return 0
-            
-            visited[r][c] = True # next if we come back again we will return 0 
-            perimeter = 0
-
-            for r_change, c_change in directions:
+    
+            visited[r][c] = True 
+            p = 0
+            for r_change, c_change in direction:
                 new_row = r + r_change
                 new_col = c + c_change
 
-                perimeter += dfs(new_row, new_col)            
-            print(perimeter)   
-            return perimeter
+                p += dfs(new_row, new_col)            
+            return p
             
-        # to find the first land
         for r in range(row):
             for c in range(col):
                 if grid[r][c] == 1:
@@ -36,5 +37,8 @@ class Solution:
         return 0
 
 
+
+        
+        
 
         
