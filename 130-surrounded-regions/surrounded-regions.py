386c47006f4a69a8868ab261525ef28479209de4
Nardos Bisrat <nardosbisrat944@gmail.com>
2026-07-14 21:05:40 +0300

    Time: 7 ms (51.27%) | Memory: 22.7 MB (20.31%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -5,37 +5,29 @@ class Solution:
         """
         row = len(board)
         col = len(board[0])
-
-        visited = [[False] * col for r in range(row)]
-        directions = [[1, 0],[0, 1],[-1, 0],[0, -1]]
-        
-
-        def inbound(r, c):
-            return 0 <= r < row and 0 <= c < col
-            
-        def change(r,c):
+        visited = [[False] * col for i in range(row)]
+        def inmiddle(r, c):
             return 0 < r < row - 1 and 0 < c < col - 1
-        
+        def inbound(r, c):
+            return 0 <= r < row and 0 <= c < col # the row and col are 0 indexed
+        direction = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         def dfs(r, c):
-            if not inbound(r, c) or board[r][c] == "X" or visited[r][c]:
+            if not inbound(r,c) or board[r][c] == "X" or visited[r][c] == True:
                 return
             visited[r][c] = True
-            
 
-            for r_change, c_change in directions:
-                new_r = r + r_change
-                new_c = c + c_change
-                dfs(new_r, new_c)
-
-                
-                
+            for dr, dc in direction:
+                newr = dr + r
+                newc = dc + c
+                dfs(newr, newc)
+        
 
         for r in range(row):
             for c in range(col):
-                if board[r][c] == "O" and not change(r, c):
-                    dfs(r, c)
-
+                if board[r][c] == "O" and not inmiddle(r,c):
+                    dfs(r,c)
         for r in range(row):
             for c in range(col):
-                if board[r][c] == "O" and not visited[r][c] :
+                if board[r][c] == "O" and not visited[r][c]:
                     board[r][c] = "X"
+        
