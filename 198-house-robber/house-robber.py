36e08807622b28cc4accd8ce6c3ba7f8696ee860
Nardos Bisrat <nardosbisrat944@gmail.com>
2026-08-12 09:38:05 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (61.36%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -6,9 +6,10 @@ class Solution:
             if n == 0:
                 return nums[n]
             if n == 1:
-                return max(nums[0], nums[1])
+                return max(nums[n - 1], nums[n])
             if n not in memo:
-                memo[n] = max(dp(n - 1), dp(n - 2) + nums[n])
+                memo[n] = max(nums[n] + dp(n - 2), dp(n - 1))
             return memo[n]
-        return dp(l - 1) 
+        return dp(l-1)
+
             
